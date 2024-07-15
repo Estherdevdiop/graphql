9d08039e1c4f3fe15dcd98ac08a470d26920719e
ndiediop <ndieyed48@gmail.com>
2024-07-15 17:11:58 +0000

    login

--- js/login.js
+++ js/login.js
@@ -8,47 +8,36 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         const credentials = btoa(`${username}:${password}`);
-       
+        const body = {
+            username: username,
+            password: password
+        };
+
         try {
             const response = await fetch('https://learn.zone01dakar.sn/api/auth/signin', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${credentials}`
+                    'Authorization': `Basic ${credentials}`,
+                    'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({
-                    provider: "username",
-                    data: {
-                        username: username,
-                        password: password
-                    }
-                })
+                body: JSON.stringify(body)
             });
-    
-            if (response.ok) {
-                const data = await response.json();
-                console.log("Réponse complète:", data);
-                
-                // Vérifiez si le JWT est bien présent et récupérez-le
-                const jwt = data ; // Adaptez selon le champ correct
-                if (jwt) {
-                    document.body.innerHTML = ''
-                    // document.body.innerHTML = 'je suis connecté'
-                    localStorage.setItem('jwt', jwt);
-                    console.log("JWT:", jwt);
-                    window.location.href = 'profil.html';
-                } else {
-                    console.error("JWT non trouvé dans la réponse.");
-                    errorMessage.textContent = 'Erreur lors de la connexion. Veuillez réessayer.';
-                }
-            } else {
-                const errorData = await response.json();
-                console.error("Erreur:", errorData);
-                errorMessage.textContent = 'Identifiant ou mot de passe incorrect.';
+
+            if (!response.ok) {
+                throw new Error('Invalid credentials');
             }
+
+            const data = await response.json();
+            console.log("data", data);
+            
+            const token = data;
+            console.log("token", token);
+
+            localStorage.setItem('jwt', token);
+            window.location.href = 'profil.html'; // Rediriger vers la page de profil
+
         } catch (error) {
-            console.error("Erreur lors de la requête:", error);
-            errorMessage.textContent = 'Une erreur est survenue. Veuillez réessayer.';
+            errorMessage.textContent = error.message;
         }
     });
 });
